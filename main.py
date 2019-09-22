23b4d41e3895167952eb7ed28f3ce14a9e416bb6
Yuchen Tian <yt523@cornell.edu>
2019-09-21 22:14:53 -0400

    some possible changes

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import logging
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
 #def hello():
     #"""Return a friendly HTTP greeting."""
     #return 'Hello World!'
 
 
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
+#@app.errorhandler(500)
+#def server_error(e):
+    #logging.exception('An error occurred during a request.')
+    #return """
+    #An internal error occurred: <pre>{}</pre>
+    #See logs for full stacktrace.
+    #""".format(e), 500
 
 
 if __name__ == '__main__':
