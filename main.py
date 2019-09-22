29ebb21ba1b9b027317c46b7aedd35c108b8d93c
Yuchen Tian <yt523@cornell.edu>
2019-09-22 00:25:22 -0400

    added new function upload

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
 import logging
+from transcribe_punct import transcribe_file_with_auto_punctuation
 
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
-#def hello():
-    #"""Return a friendly HTTP greeting."""
-    #return 'Hello World!'
+@app.route('/transc')
+def transc():
+    return transcribe_file_with_auto_punctuation('commercial_mono.wav')
 
-
-#@app.errorhandler(500)
-#def server_error(e):
-    #logging.exception('An error occurred during a request.')
-    #return """
-    #An internal error occurred: <pre>{}</pre>
-    #See logs for full stacktrace.
-    #""".format(e), 500
-
+@app.errorhandler(500)
+def server_error(e):
+    logging.exception('An error occurred during a request.')
+    return """
+    An internal error occurred: <pre>{}</pre>
+    See logs for full stacktrace.
+    """.format(e), 500
 
 if __name__ == '__main__':
     # This is used when running locally. Gunicorn is used to run the
-    # application on Google App Engine. See entrypoint in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    # application on Google App Engine. See entrypoint in app.yaml. host='127.0.0.1'
+    app.run(port=8080, debug=True)
---
+++ upload.py
@@ -0,0 +1,30 @@
+from google.cloud import storage
+def upload(file):
+    public_url = upload_file(file)
+    return public_url
+    print(public_url)
+
+def upload_file(file):
+    """
+    Uploads a file to a given Cloud Storage bucket and returns the public url
+    to the new object.
+    """
+    client = storage.Client()
+    # The name for the new bucket
+    bucket_name = 'my-new-bucket'
+    # Creates the new bucket
+    bucket = client.create_bucket(bucket_name)
+    blob = bucket.blob(file)
+    #blob.upload_from_string(
+        #file_stream,
+        #content_type=content_type)
+
+    #url = blob.public_url
+
+
+
+
+
+
+
+upload('commercial_mono.wav')
